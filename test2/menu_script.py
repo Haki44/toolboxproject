4b61f70ecd900c85a8cf4f8f14a7fc1f36a5ffa6
Maxence Bannier <70164079+Haki44@users.noreply.github.com>
2024-08-06 17:14:18 +0200

    add test_pwd to the main script

--- test2/menu_script.py
+++ test2/menu_script.py
@@ -1,11 +1,16 @@
 import subprocess
 from art import tprint
+from scripts.test_pwd import test_pwd
 
 def menu():
     # Affiche le menu
-    print("\n     Menu script test :")
+    print("\n     Menu toolbox :")
     print("         [1] - Scan de ports")
-    print("         [2] - Scan de vulnérabilités\n")
+    print("         [2] - Scan de vulnérabilités")
+    print("         [3] - Exploitation de vulnérabilités")
+    print("         [4] - Test d'authentification")
+    print("         [5] - Analyse de la sécurité des mots de passe")
+    print("         [6] - Générer un rapport\n")
 
     choix = input("Veuillez choisir une option: ")
 
@@ -13,7 +18,11 @@ def menu():
     if choix == "1":
         subprocess.run(['python3', 'script_n1.py'])  # Exécuter le script n1 avec le nom de fichier en argument
     elif choix == "2":
-        subprocess.run(['python3', 'script_n2.py'])  # Exécuter le script n2 avec le nom de fichier en argument
+        subprocess.run(['python3', 'scripts/scan_vuln.py'])  # Exécuter le script n2 avec le nom de fichier en argument
+    elif choix == "5":
+        test_pwd()
+        input("Appuyez sur entrer pour retourner au menu")
+        menu()
     else:
         print("Option non valide.")
 
@@ -22,4 +31,5 @@ if __name__ == "__main__":
     print("=" * 82)
     tprint("  Cyber Toolbox  ")
     print("=" * 82)
+    print("                                   [version 1.0]")
     menu()
